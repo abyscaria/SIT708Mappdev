137f7e58e6fc667c1b0db49256e599e70e0c7b59
Aby Scaria <aby.scaria@gmail.com>
2022-05-10 15:15:42 +1000

    Updated app files and new Kotlin samples

--- appBasics/app/src/main/java/com/examples/appbasics/MainActivity.kt
+++ appBasics/app/src/main/java/com/examples/appbasics/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.examples.appbasics
 
 import android.content.Intent
+import android.content.Intent.ACTION_SEND
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
 
-const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
+const val EXTRA_MESSAGE = "You won a  lottery $$$$"
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +18,9 @@ class MainActivity : AppCompatActivity() {
         val editText = findViewById<EditText>(R.id.editText)
         val message = editText.text.toString()
         val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            action = ACTION_SEND
             putExtra(EXTRA_MESSAGE, message)
+            type = "text/bold"
         }
         startActivity(intent)
     }
