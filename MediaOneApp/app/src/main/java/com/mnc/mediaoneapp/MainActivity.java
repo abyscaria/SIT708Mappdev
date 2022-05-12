61a490cadc341057a86a2355058893d5aaf497c4
abyscaria <aby.scaria@gmail.com>
2022-05-12 19:25:36 +1000

    MediaOne
    new app

---
+++ MediaOneApp/app/src/main/java/com/mnc/mediaoneapp/MainActivity.java
@@ -0,0 +1,94 @@
+package com.mnc.mediaoneapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    // creating a variable for
+    // button and media player
+    Button playBtn, pauseBtn;
+    MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // initializing our buttons
+        playBtn = findViewById(R.id.button1);
+        pauseBtn = findViewById(R.id.button2);
+
+        // setting on click listener for our play and pause buttons.
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // calling method to play audio.
+                playAudio();
+            }
+        });
+
+        pauseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // checking the media player
+                // if the audio is playing or not.
+                if (mediaPlayer.isPlaying()) {
+                    // pausing the media player if media player
+                    // is playing we are calling below line to
+                    // stop our media player.
+                    mediaPlayer.stop();
+                    mediaPlayer.reset();
+                    mediaPlayer.release();
+
+                    // below line is to display a message
+                    // when media player is paused.
+                    Toast.makeText(MainActivity.this, "Audio has been paused", Toast.LENGTH_SHORT).show();
+                } else {
+                    // this method is called when media
+                    // player is not playing.
+                    Toast.makeText(MainActivity.this, "Audio has not played", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void playAudio() {
+
+        //String audioUrl = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3";
+        String audioUrl = "http://www.hubharp.com/web_sound/BachGavotte.mp3";
+
+        // initializing media player
+        mediaPlayer = new MediaPlayer();
+
+        // below line is use to set the audio
+        // stream type for our media player.
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        // below line is use to set our
+        // url to our media player.
+        try {
+            mediaPlayer.setDataSource(audioUrl);
+            // below line is use to prepare
+            // and start our media player.
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // below line is use to display a toast message.
+        Toast.makeText(this, "Audio started playing..", Toast.LENGTH_SHORT).show();
+    }
+}
